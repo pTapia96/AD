335e16fba7a1d601527a2fc103f429731d861c67
pTapia96 <pabrod@alu.iesserpis.org>
2019-09-25 08:45:30 +0200

    Añadido CMySql, en desarrollo. Run files en .gitignore

---
+++ proyectos_sql/dbprueba.sql
@@ -0,0 +1,7 @@
+create table categoria (
+	id serial primary key,
+	nombre varchar(50) not null unique
+);
+insert into categoria (nombre) values ('categoría 1');
+insert into categoria (nombre) values ('categoría 2');
+insert into categoria (nombre) values ('categoría 3');
